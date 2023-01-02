2eec6b1b84db0d8b011b089653657dcaa6ddb90f
dkpark10 <dkpark10@naver.com>
2023-01-02 18:27:45 +0900

    [feat]: 커밋린트 추가

---
+++ commitlint.config.cjs
@@ -0,0 +1,34 @@
+const commitTypes = ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refac', 'revert', 'style', 'test'];
+
+module.exports = {
+  rules: {
+    'type-case': [2, 'always', 'lower-case'],
+    'type-enum': [
+      2,
+      'always',
+      commitTypes.map((type) => `[${type}]`),
+    ],
+  },
+  plugins: [
+    {
+      rules: {
+        'type-enum': (parsed) => {
+          const { header } = parsed;
+
+          if (header[0] !== '[') {
+            return [false, '커밋 타입에 대괄호를 삽입하세요.'];
+          }
+
+          const indexOfcloseBrakcet = header.indexOf(']');
+          const commitType = header.slice(1, indexOfcloseBrakcet);
+
+          if (commitTypes.includes(commitType) === false) {
+            return [false, '올바르지 않은 커밋타입 입니다.'];
+          }
+
+          return [true, ''];
+        },
+      },
+    },
+  ],
+};
