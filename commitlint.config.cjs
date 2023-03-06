214143df72b7517cf78ef7a4b227c11bbba9ca00
dkpark10 <dkpark10@naver.com>
2023-03-06 22:52:27 +0900

    chore: 커밋린트 설정 변경

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,34 +1,36 @@
-const commitTypes = ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refac', 'revert', 'style', 'test'];
+// const commitTypes = ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refac', 'revert', 'style', 'test'];
 
-module.exports = {
-  rules: {
-    'type-case': [2, 'always', 'lower-case'],
-    'type-enum': [
-      2,
-      'always',
-      commitTypes.map((type) => `[${type}]`),
-    ],
-  },
-  plugins: [
-    {
-      rules: {
-        'type-enum': (parsed) => {
-          const { header } = parsed;
+// module.exports = {
+//   rules: {
+//     'type-case': [2, 'always', 'lower-case'],
+//     'type-enum': [
+//       2,
+//       'always',
+//       commitTypes.map((type) => `[${type}]`),
+//     ],
+//   },
+//   plugins: [
+//     {
+//       rules: {
+//         'type-enum': (parsed) => {
+//           const { header } = parsed;
 
-          if (header[0] !== '[') {
-            return [false, '커밋 타입에 대괄호를 삽입하세요.'];
-          }
+//           if (header[0] !== '[') {
+//             return [false, '커밋 타입에 대괄호를 삽입하세요.'];
+//           }
 
-          const indexOfcloseBrakcet = header.indexOf(']');
-          const commitType = header.slice(1, indexOfcloseBrakcet);
+//           const indexOfcloseBrakcet = header.indexOf(']');
+//           const commitType = header.slice(1, indexOfcloseBrakcet);
 
-          if (commitTypes.includes(commitType) === false) {
-            return [false, '올바르지 않은 커밋타입 입니다.'];
-          }
+//           if (commitTypes.includes(commitType) === false) {
+//             return [false, '올바르지 않은 커밋타입 입니다.'];
+//           }
 
-          return [true, ''];
-        },
-      },
-    },
-  ],
-};
+//           return [true, ''];
+//         },
+//       },
+//     },
+//   ],
+// };
+
+module.exports = { extends: ['@commitlint/config-conventional'] };
---
+++ src/components/testgc.tsx
@@ -0,0 +1,15 @@
+import { arrayBuffer } from 'node:stream/consumers';
+import React, { useEffect } from 'react';
+
+const arr = [];
+
+export default function TestGc() {
+  useEffect(() => {
+    arr.push(new Array(100000).fill(1));
+    // return (() => {
+    //   arr = null;
+    // });
+  }, []);
+
+  return <div>테수투</div>;
+}
